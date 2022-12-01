0b02ecf29140d39f312c88d508b8e973b5bcb950
Neculita Cristina <cristina.neculita@scania.com>
2022-12-01 06:15:12 +0100

    day 1

---
+++ 2022/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Runtime.CompilerServices;
+
+var rez = 0;
+var listr = new List<long>();
+string[] lines = System.IO.File.ReadAllLines("input.txt");
+long s = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+
+        listr.Add(s);
+        s = 0;
+    }
+    else
+    {
+        int.TryParse(line, out int r);
+        s += r;
+    }
+}
+
+ s = 0;
+var m = listr.Max();
+s += (long)m;
+
+listr.Remove(m);
+m = listr.Max();
+s += (long)m;
+
+listr.Remove(m);
+m = listr.Max();
+s += (long)m;
+
+listr.Remove(m);
+
+Console.WriteLine("Hello, World!  {0}",s);
