4ab352207b34751c0caa0bb59f79f260caef32ef
Neculita Cristina <cristina.neculita@scania.com>
2022-12-14 06:50:06 +0100

    day 14

--- 2022/2022/Program.cs
+++ 2022/2022/Program.cs
@@ -17,203 +17,114 @@ namespace Advent2022
 {
 
 
-
-    class Program
+    
+    public class Program
     {
-        class Str : IComparable<Str>
-        {
-            public string It { get; set; }
-
-            public int CompareTo(Str? other)
-            {
-                if (ReferenceEquals(this, other)) return 0;
-                if (ReferenceEquals(null, other)) return 1;
-                var c = CompareNou(this.It, other.It);
-                if (!c.HasValue)
-                    return 0;
-                if (c.Value)
-                    return -1;
-                return 1;
-            }
-
-            public Str(string it)
-            {
-                It = it;
-            }
-        }
-
         static void Main(string[] args)
         {
             double rez = 0;
+            var map = new int[6000, 500];
 
-            string[] lines = System.IO.File.ReadAllLines("input1.txt");
-            var listS = new List<Str>();
-            int i = 1;
-            for (int j = 0; j < lines.Length; j += 3)
+            string[] lines = File.ReadAllLines("input1.txt");
+            foreach (var line in lines)
             {
-                var s1 = lines[j];
-                var s2 = lines[j + 1];
-                listS.Add(new Str(s1));
-                listS.Add(new Str(s2));
+                var x =line.Split(" -> ");
 
-                //if (CompareNou(lines[j], lines[j + 1]).Value)
-                //    rez += i;
-                // i++;
-            }
-
-            var s11 = new Str("[[2]]");
-            var s12 = new Str("[[6]]");
-            listS.Add(s11);
-            listS.Add(s12);
-            var ind1 = 0;
-            var ind2 = 0;
-            //listS.Sort();
-
-            foreach (var s in listS)
-            {
-                if (s.CompareTo(s11) < 0)
-                    ind1++;
-            }
-
-            foreach (var s in listS)
-            {
-                if (s.CompareTo(s12) < 0)
-                    ind2++;
-            }
-
-            rez = (ind1 + 1) * (ind2 + 1);
-
-            Console.WriteLine("sum " + rez);
-        }
-
-        static bool? CompareNou(string s1, string s2)
-        {
-            if (s1 == "" && s2 != "")
-                return true;
-            if (s1 != "" && s2 == "")
-                return false;
-            if (isInteger(s1) && isInteger(s2))
-            {
-                var n1 = GetInteger(s1);
-                var n2 = GetInteger(s2);
-                if (n1 < n2)
-                    return true;
-                if (n1 > n2)
-                    return false;
-                if (n1 == n2)
-                    return null;
-            }
-
-            if (isListNou(s1) && isListNou(s2))
-            {
-                var index1 = 1;
-                var index2 = 1;
-                while (true)
+                for (int i = 0; i < x.Length-1; i++)
                 {
-                    string e1 = takeNextElNou(s1, index1);
-                    string e2 = takeNextElNou(s2, index2);
-                    var x = CompareNou(e1, e2);
-                    if (x.HasValue && x.Value)
-                        return true;
-                    if (x.HasValue && !x.Value)
-                        return false;
+                    var y1 = x[i].Split(",");
+                    var y2 = x[i + 1].Split(",");
+                    int.TryParse(y1[0], out int y11);
+                    int.TryParse(y1[1], out int y12);
+                    int.TryParse(y2[0], out int y21);
+                    int.TryParse(y2[1], out int y22);
+                    var xmin = Math.Min(y11, y21);
+                    var xmax = Math.Max(y11, y21);
+                    var ymin = Math.Min(y12, y22);
+                    var ymax = Math.Max(y12, y22);
 
-                    index1 += e1.Length;
-                    index2 += e2.Length;
-
-                    if (index1 == s1.Length - 1 && index2 == s2.Length - 1)
-                        return null;
-                    if (index1 == s1.Length - 1)
-                        return true;
-                    if (index2 == s2.Length - 1)
-                        return false;
-
-                }
-            }
-            else
-            {
-                if (isListNou(s1) && isInteger(s2))
-                {
-                    var c2 = ConvertToList(s2);
-                    return CompareNou(s1, c2);
-                }
-
-                if (isListNou(s2) && isInteger(s1))
-                {
-                    var c1 = ConvertToList(s1);
-                    return CompareNou(c1, s2);
-                }
-            }
-
-            return null;
-        }
-
-        private static string takeNextElNou(string s1, int index1)
-        {
-
-            var pd = 0;
-            if (s1[index1] == ',')
-                index1++;
-            var i = index1;
-            if (s1[index1] == '[')
-            {
-                while (true)
-                {
-                    if (s1[i] == '[')
-                        pd++;
-                    if (s1[i] == ']')
+                    for (int j = xmin; j <=xmax; j++)
                     {
-                        if (pd == 1)
-                            break;
-                        pd--;
+                        for (int k = ymin; k <=ymax; k++)
+                        {
+                            map[j, k] = 1;
+                        }
                     }
-
-                    i++;
-                    if (i == s1.Length)
-                        break;
                 }
-
-                i++;
-                var ind2 = index1;
-                return new string(s1[ind2..i]);
             }
 
-            if (isInteger(s1[index1].ToString()))
+            var maxjos = 0;
+            for (int i = 0; i < 6000; i++)
             {
-                while (s1[i] != ',' && s1[i] != ']' && i < s1.Length - 1)
-                    i++;
-                return s1[index1..i];
+                for (int j = 0; j < 500; j++)
+                {
+                    if (map[i, j] == 1)
+                    {
+                        if(j>maxjos)
+                            maxjos = j;
+                    }
+                }
             }
 
-            return "";
+
+            for (int i = 0; i < 6000; i++)
+            {
+                map[i, maxjos+2] = 1;
+            }
+
+            var xs = 500;
+            var ys = 0;
+            var sandInMap = false;
+            var numbersand = 0;
+            do
+            {
+                numbersand++;
+                sandInMap = PourSand(xs, ys, map, maxjos);
+                
+            } while (sandInMap);
+
+            numbersand--;
+
+            Console.WriteLine("sum " + numbersand);
         }
 
-        private static string ConvertToList(string s2)
+        private static bool PourSand(int xsi, int ysi, int[,] map, int maxjos)
         {
-            return "[" + s2 + "]";
-        }
+            var xs = xsi;
+            var ys = ysi;
+            while (true)
+            {
+               //down
+              
+               if (map[xs, ys+1] != 1 && map[xs, ys+1] != 2)
+               {
+                   ys++;
+                   //if(ys<=maxjos)
+                        continue;
+               }
 
-        private static bool isListNou(string s1)
-        {
-            if (s1.Length == 0)
-                return false;
-            return (s1[0] == '[');
-        }
+               if (map[xs - 1, ys + 1] != 1 && map[xs - 1, ys + 1] != 2)
+               {
+                   ys++;
+                   xs--;
+                   //if (ys <= maxjos)
+                       continue;
+               }
+               if (map[xs + 1, ys + 1] != 1 && map[xs + 1, ys + 1] != 2)
+               {
+                   ys++;
+                   xs++;
+                   //if (ys <= maxjos)
+                        continue;
+               }
+               
+                map[xs,ys] = 2;
+                if (map[500, 0] == 2)
+                    return false;
+                return true;
 
-        private static int GetInteger(string s1)
-        {
-            s1 = s1.TrimEnd(',');
-            int.TryParse(s1, out int n);
-            return n;
+            }
         }
-
-        private static bool isInteger(string s1)
-        {
-            if (s1.Length == 0)
-                return false;
-            return (s1[0] >= '0' && s1[0] <= '9');
-        }
-
     }
 
 }
