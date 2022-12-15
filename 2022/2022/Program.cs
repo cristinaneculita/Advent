1ecf17874954abc0b70eb1929c9689c779068a57
Neculita Cristina <cristina.neculita@scania.com>
2022-12-15 15:45:35 +0100

    day 15

--- 2022/2022/Program.cs
+++ 2022/2022/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,114 +12,341 @@ using System.Runtime.CompilerServices;
 
 namespace Advent2022
 {
+    class Point
+    {
+        public long x { get; set; }
+        public long y { get; set; }
+
+        public Point(long x, long y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Item
+    {
+        public Point Sensor { get; set; }
+        public Point Beacon { get; set; }
+        public long Dist { get; set; }
+        public Item(Point sensor, Point beacon)
+        {
+            Sensor = sensor;
+            Beacon = beacon;
+            Dist = Getdist(Beacon, Sensor);
+        }
+
+        private long Getdist(Point item1, Point item2)
+        {
+            return (Math.Abs(item1.x - item2.x) + Math.Abs(item1.y - item2.y));
+        }
+    }
 
 
-    
     public class Program
     {
         static void Main(string[] args)
         {
             double rez = 0;
-            var map = new int[6000, 500];
-
+            var items = new List<Item>();
             string[] lines = File.ReadAllLines("input1.txt");
             foreach (var line in lines)
             {
-                var x =line.Split(" -> ");
+                var x = line.Split(" ");
+                long.TryParse(x[0], out long x0);
+                long.TryParse(x[1], out long x1);
+                long.TryParse(x[2], out long x2);
+                long.TryParse(x[3], out long x3);
 
-                for (int i = 0; i < x.Length-1; i++)
+                var item = new Item(new Point(x0, x1), new Point(x2, x3));
+                items.Add(item);
+            }
+
+            long minx = int.MaxValue, miny = int.MaxValue, maxx = int.MinValue, maxy = int.MinValue;
+
+            foreach (var item in items)
+            {
+                if (item.Beacon.x < minx)
+                    minx = item.Beacon.x;
+                if (item.Beacon.y < miny)
+                    miny = item.Beacon.y;
+                if (item.Beacon.x > maxx)
+                    maxx = item.Beacon.x;
+                if (item.Beacon.y > maxy)
+                    maxy = item.Beacon.y;
+
+                if (item.Sensor.x < minx)
+                    minx = item.Sensor.x;
+                if (item.Sensor.y < miny)
+                    miny = item.Sensor.y;
+                if (item.Sensor.x > maxx)
+                    maxx = item.Sensor.x;
+                if (item.Sensor.y > maxy)
+                    maxy = item.Sensor.y;
+
+            }
+
+            //var shift = -1440697;
+            ////var shift = 10;
+            //// var shiftx = 20;
+            //var shiftx = 2000000;
+            //foreach (var item in items)
+            //{
+            //    item.Sensor.y += shift;
+            //    item.Beacon.y += shift;
+            //    item.Beacon.x += shiftx;
+            //    item.Sensor.x += shiftx;
+            //}
+
+
+            //var notallowed = new bool[7000000];
+            ////var adjustedy = 2000000 + shift;
+            ////var adjustedy = 10;
+            //for (int adjustedy = -1350400; adjustedy <= 4000000 + shift; adjustedy++)
+            //{
+            //    if (adjustedy % 100 == 0)
+            //    {
+            //        Console.WriteLine(adjustedy);
+            //    }
+
+            //    // for (int i = 0 + shiftx; i < 4000000 + shiftx; i++)
+
+            //    foreach (var item in items)
+            //    {
+            //        //var dist = Getdist(item.Beacon, item.Sensor);
+            //        if (AroundTarget(item, item.Dist, adjustedy))
+            //        {
+            //            var difFromTarget = Math.Abs(adjustedy - item.Sensor.y);
+            //            var distleft = item.Dist - difFromTarget;
+            //            MarkNotAllowedNew(distleft, notallowed, adjustedy, item);
+            //        }
+
+            //        if (item.Beacon.y == adjustedy)
+            //            notallowed[item.Beacon.x] = true;
+            //        if (item.Sensor.y == adjustedy)
+            //            notallowed[item.Sensor.x] = true;
+            //        //MarkNotAllowed(item, notallowed, adjustedy);
+            //    }
+
+
+
+
+            //    for (int i = 0 + shiftx; i < 4000000 + shiftx; i++)
+            //    {
+            //        if (notallowed[i] == false)
+            //        {
+            //            Console.WriteLine("x= " + (i - shiftx));
+            //            Console.WriteLine("y= " + (adjustedy - shift));
+            //        }
+
+            //        notallowed[i] = false;
+
+
+            //    }
+            //    //Console.WriteLine(adjustedy + " ");
+            //}
+            //foreach (var item in items)
+            //{
+            //    var dist = Getdist(item.Beacon, item.Sensor);
+            //    if (AroundTarget(item, dist, adjustedy))
+            //    {
+            //        var difFromTarget = Math.Abs(adjustedy - item.Sensor.y);
+            //        var distleft = dist - difFromTarget;
+            //        MarkNotAllowedNew(distleft, notallowed, adjustedy, item);
+            //    }
+
+            //    //MarkNotAllowed(item, notallowed, adjustedy);
+            //}
+
+            //foreach (var item in items)
+            //{
+            //    if (item.Beacon.y == adjustedy)
+            //        notallowed[item.Beacon.x] = false;
+            //}
+
+
+            //for (int i = 0; i < 7000000; i++)
+            //{
+            //    if (notallowed[i])
+            //        rez++;
+            //}
+            //distanta fata de toate sursele este mai mare decat distanta itemului
+            var point = new Point(0, 0);
+            var howmany = 0;
+            var tot = items.Count;
+            var marked = new int[4000000];
+            var cand = new List<Point>();
+            long xm, ym;
+
+            foreach (var item in items)
+            {
+                var i = item.Dist + 1;
+                for (int j = 0; j < i; j++)
                 {
-                    var y1 = x[i].Split(",");
-                    var y2 = x[i + 1].Split(",");
-                    int.TryParse(y1[0], out int y11);
-                    int.TryParse(y1[1], out int y12);
-                    int.TryParse(y2[0], out int y21);
-                    int.TryParse(y2[1], out int y22);
-                    var xmin = Math.Min(y11, y21);
-                    var xmax = Math.Max(y11, y21);
-                    var ymin = Math.Min(y12, y22);
-                    var ymax = Math.Max(y12, y22);
+                    xm = item.Sensor.x - j;
+                    ym = item.Sensor.y - i + j;
+                    //if (adsustedy == ym) notallowed[xm] = true;
+                    //if (item.Beacon.x == xm && item.Beacon.y == ym)
+                    //    found = true;
+                    cand.Add(new Point(xm, ym));
+                }
 
-                    for (int j = xmin; j <=xmax; j++)
-                    {
-                        for (int k = ymin; k <=ymax; k++)
-                        {
-                            map[j, k] = 1;
-                        }
-                    }
+                //stanga-jos
+                for (int j = 0; j < i; j++)
+                {
+                    xm = item.Sensor.x - i + j;
+                    ym = item.Sensor.y - i + j;
+                    //if (adsustedy == ym) notallowed[xm] = true;
+                    //if (item.Beacon.x == xm && item.Beacon.y == ym)
+                    //    found = true;
+                    cand.Add(new Point(xm, ym));
+                }
+
+                //jos-dr
+                for (int j = 0; j < i; j++)
+                {
+                    xm = item.Sensor.x + j;
+                    ym = item.Sensor.y + i - j;
+                    //if (adsustedy == ym) notallowed[xm] = true;
+                    //if (item.Beacon.x == xm && item.Beacon.y == ym)
+                    //    found = true;
+                    cand.Add(new Point(xm, ym));
+                }
+
+                //dreapta-sus
+                for (int j = 0; j < i; j++)
+                {
+                    xm = item.Sensor.x + i - j;
+                    ym = item.Sensor.y - j;
+                    //if (adsustedy == ym) notallowed[xm] = true;
+                    //if (item.Beacon.x == xm && item.Beacon.y == ym)
+                    //    found = true;
+                    cand.Add(new Point(xm, ym));
                 }
             }
+            //for (int i = 0; i < 4000000; i++)
+            //{
+            //    foreach (var item in items)
+            //    {
+            //        var difx = Math.Abs(item.Sensor.x - i);
+            //        for (int j = 0; j < 4000000; j++)
+            //        {
+            //            if ((difx + Math.Abs(item.Sensor.y - j) > item.Dist))
+            //                marked[j]++;
+            //        }
+            //    }
 
-            var maxjos = 0;
-            for (int i = 0; i < 6000; i++)
+            //    for (int j = 0; j < 4000000; j++)
+            //    {
+            //        //if (marked[j] == 35)
+            //        //    Console.WriteLine("Ceva");
+            //        marked[j] = 0;
+            //    }
+            //}
+
+            foreach (var c in cand)
             {
-                for (int j = 0; j < 500; j++)
+                howmany = 0;
+                foreach (var item in items)
                 {
-                    if (map[i, j] == 1)
-                    {
-                        if(j>maxjos)
-                            maxjos = j;
-                    }
+                    if (Getdist(item.Sensor, c) > item.Dist)
+                        howmany++;
                 }
+
+                if (howmany == tot)
+                {
+                    if (c.x is >= 0 and <= 4000000 && c.y >=0 && c.y <=4000000)
+                    Console.WriteLine($"x= {c.x} y={c.y}");
+                }
+
             }
+        
 
+        //for (int j = 0; j < 4000000; j++)
+                //{
+                //    point.y = j;
+                //    howmany = 0;
+                //    foreach (var item in items)
+                //    {
+                //        if (Getdist(item.Sensor, point) > item.Dist)
+                //            howmany++;
+                //    }
 
-            for (int i = 0; i < 6000; i++)
-            {
-                map[i, maxjos+2] = 1;
+                //    if (howmany == tot)
+                //        Console.WriteLine($"x= {i} y={j}");
+                //}
             }
+           // Console.WriteLine("sum " + rez);
+        
 
-            var xs = 500;
-            var ys = 0;
-            var sandInMap = false;
-            var numbersand = 0;
-            do
-            {
-                numbersand++;
-                sandInMap = PourSand(xs, ys, map, maxjos);
-                
-            } while (sandInMap);
-
-            numbersand--;
-
-            Console.WriteLine("sum " + numbersand);
+        
+        private static void MarkNotAllowedNew(long distleft, bool[] notallowed, int adjustedy, Item item)
+        {
+            for (long i = item.Sensor.x - distleft; i <= item.Sensor.x + distleft; i++)
+                notallowed[i] = true;
         }
 
-        private static bool PourSand(int xsi, int ysi, int[,] map, int maxjos)
+        private static bool AroundTarget(Item item, long dist, int adjustedy)
         {
-            var xs = xsi;
-            var ys = ysi;
+            var ymax = item.Sensor.y + dist;
+            var ymin = item.Sensor.y - dist;
+            if (ymin <= adjustedy && adjustedy <= ymax)
+                return true;
+            return false;
+        }
+
+        private static long Getdist(Point item1, Point item2)
+        {
+            return (Math.Abs(item1.x - item2.x) + Math.Abs(item1.y - item2.y));
+        }
+
+        private static void MarkNotAllowed(Item item, bool[] notallowed, int adsustedy)
+        {
+            var i = 1;
+            long xm, ym;
+            var found = false;
             while (true)
             {
-               //down
-              
-               if (map[xs, ys+1] != 1 && map[xs, ys+1] != 2)
-               {
-                   ys++;
-                   //if(ys<=maxjos)
-                        continue;
-               }
+                //sus-stg
+                for (int j = 0; j < i; j++)
+                {
+                    xm = item.Sensor.x - j;
+                    ym = item.Sensor.y - i + j;
+                    if (adsustedy == ym) notallowed[xm] = true;
+                    if (item.Beacon.x == xm && item.Beacon.y == ym)
+                        found = true;
+                }
+                //stanga-jos
+                for (int j = 0; j < i; j++)
+                {
+                    xm = item.Sensor.x - i + j;
+                    ym = item.Sensor.y - i + j;
+                    if (adsustedy == ym) notallowed[xm] = true;
+                    if (item.Beacon.x == xm && item.Beacon.y == ym)
+                        found = true;
+                }
+                //jos-dr
+                for (int j = 0; j < i; j++)
+                {
+                    xm = item.Sensor.x + j;
+                    ym = item.Sensor.y + i - j;
+                    if (adsustedy == ym) notallowed[xm] = true;
+                    if (item.Beacon.x == xm && item.Beacon.y == ym)
+                        found = true;
+                }
+                //dreapta-sus
+                for (int j = 0; j < i; j++)
+                {
+                    xm = item.Sensor.x + i - j;
+                    ym = item.Sensor.y - j;
+                    if (adsustedy == ym) notallowed[xm] = true;
+                    if (item.Beacon.x == xm && item.Beacon.y == ym)
+                        found = true;
+                }
 
-               if (map[xs - 1, ys + 1] != 1 && map[xs - 1, ys + 1] != 2)
-               {
-                   ys++;
-                   xs--;
-                   //if (ys <= maxjos)
-                       continue;
-               }
-               if (map[xs + 1, ys + 1] != 1 && map[xs + 1, ys + 1] != 2)
-               {
-                   ys++;
-                   xs++;
-                   //if (ys <= maxjos)
-                        continue;
-               }
-               
-                map[xs,ys] = 2;
-                if (map[500, 0] == 2)
-                    return false;
-                return true;
-
+                if (found)
+                    break;
+                i++;
             }
         }
     }
